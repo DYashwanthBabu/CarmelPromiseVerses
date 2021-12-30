13ddb0e4c079697a29c2edb152154923dd69e152
Yashwanth_Babu <yashwanthbabumk@gmail.com>
2021-12-31 00:37:02 +0530

    first may be

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import request
+from flask import render_template, jsonify
+import pandas as pd
+import random
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+
+    return render_template('Home.html')
+
+@app.route("/promise_verse", methods=['GET', 'POST'])
+def promise_verse():
+    # allStock = pd.read_csv('NIFTY50_all.csv')
+    # stock_symbols=allStock.Symbol.unique()
+    if(request.method =='POST'):
+        filename = request.form
+        namesList = []
+        valuesList = []
+        df = pd.read_csv("final.csv")
+        for i in filename.keys():
+            ranNum = random.randint(0,171)
+            li = df.loc[ranNum].tolist()
+            verse = li[0] + ": "+li[1] 
+            namesList.append(filename[i])
+            valuesList.append(verse)
+        return render_template('PromiseVerse.html', key=namesList, val=valuesList)
+
+    return render_template('NPromiseVerse.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
